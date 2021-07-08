b694ff8f2cd54c59a3dc5598f08f50dddc45876b
andrellgrillo <andrell.grillo@gmail.com>
2021-07-08 15:48:41 -0300

    first commit

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+  if (categoriesRepository.findByName(name)) {
+    return response
+      .status(400)
+      .json({ error: `A categoria ${name} já existe` });
+  }
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+});
+categoriesRoutes.get("/", (request, response) => {
+  const categories = categoriesRepository.list();
+
+  return response.json(categories);
+});
+
+export { categoriesRoutes };
