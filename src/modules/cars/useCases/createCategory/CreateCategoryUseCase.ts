d952919b67176fec96fedeef17da8de3cb30de40
andrellgrillo <andrell.grillo@gmail.com>
2021-07-16 09:09:09 -0300

    criando banco de dados e tabela de categorias

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -8,8 +8,9 @@ interface IRequest {
 class CreateCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
-  execute({ name, description }: IRequest): void {
-    if (this.categoriesRepository.findByName(name)) {
+  async execute({ name, description }: IRequest): Promise<void> {
+    const categoryExist = await this.categoriesRepository.findByName(name);
+    if (categoryExist) {
       throw new Error(" Category already exist!");
     }
     this.categoriesRepository.create({ name, description });
