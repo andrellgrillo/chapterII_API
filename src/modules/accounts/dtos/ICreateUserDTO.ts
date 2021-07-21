32632f18b87c47f93e8a2d1de2c17141fca92a6f
andrellgrillo <andrell.grillo@gmail.com>
2021-07-21 10:25:48 -0300

    correção no upload de avatar

--- src/modules/accounts/dtos/ICreateUserDTO.ts
+++ src/modules/accounts/dtos/ICreateUserDTO.ts
@@ -3,6 +3,8 @@ interface ICreatedUsersDTO {
   password: string;
   email: string;
   driver_license: string;
+  id?: string;
+  avatar?: string;
 }
 
 export { ICreatedUsersDTO };
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 
 import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
@@ -14,6 +15,7 @@ const updateUserAvatarController = new UpdateUserAvatarController();
 usersRoutes.post("/", createUserController.handle);
 usersRoutes.patch(
   "/avatar",
+  ensureAuthenticated,
   uploadAvatar.single("avatar"),
   updateUserAvatarController.handle
 );
